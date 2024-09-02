c908657bda48a593958c8a2f2047f41f87fb66ac
ZefanyaDelvin <delvinzefanya@gmail.com>
2024-09-02 21:28:13 +0700

    add markdown and delete

--- cypress/e2e/newtours.feature
+++ cypress/e2e/newtours.feature
@@ -5,3 +5,21 @@ Scenario: User adds a new to-do item
     When the user enters a new to-do item "Buy groceries"
     And the user clicks the add button
     Then the new to-do item "Buy groceries" should be added to the list
+
+
+/* Markdone */
+Feature: Mark a to-do item as completed
+
+Scenario: User marks a to-do item as completed
+  Given the user has a to-do item "Buy groceries"
+    When the user marks the to-do item "Buy groceries" as completed
+    Then the to-do item "Buy groceries" should be marked as completed
+    
+/* Delete */
+Feature: Delete a to-do item
+
+Scenario: User deletes a to-do item
+  Given the user has a to-do item "Buy groceries"
+    When the user deletes the to-do item "Buy groceries"
+    Then the to-do item "Buy groceries" should be removed from the list
+
--- cypress/e2e/newtours/add-todo.js
+++ cypress/e2e/newtours/add-todo.js
@@ -1,5 +1,6 @@
 import { Given, When, Then } from 'cypress-cucumber-preprocessor/steps';
 
+/************************** Add **************************/
 Given('the user is on the to-do page', () => {
   cy.visit('http://localhost:3000/');
 });
@@ -15,3 +16,37 @@ When('the user clicks the add button', () => {
 Then('the new to-do item {string} should be added to the list', (todo) => {
   cy.get('[data-cy=todo-list]').should('contain', todo);
 });
+
+/************************** Markdone **************************/
+/*
+Given('the user has a to-do item {string}', (todo) => {
+  cy.visit('http://localhost:3000/');
+  cy.get('[data-cy=new-todo-input]').type(todo);
+  cy.get('[data-cy=add-todo-button]').click();
+});
+
+When('the user marks the to-do item {string} as completed', (todo) => {
+  cy.get(`[data-cy=complete-todo-${todo}]`).click();
+});
+
+Then('the to-do item {string} should be marked as completed', (todo) => {
+  cy.get(`[data-cy=todo-item-${todo}]`).should('have.class', 'completed');
+});
+*/
+
+/************************** Delete **************************/
+/*
+Given('the user has a to-do item {string}', (todo) => {
+  cy.visit('/');
+  cy.get('[data-cy=new-todo-input]').type(todo);
+  cy.get('[data-cy=add-todo-button]').click();
+});
+
+When('the user deletes the to-do item {string}', (todo) => {
+  cy.get(`[data-cy=delete-todo-${todo}]`).click();
+});
+
+Then('the to-do item {string} should be removed from the list', (todo) => {
+  cy.get('[data-cy=todo-list]').should('not.contain', todo);
+});
+*/
